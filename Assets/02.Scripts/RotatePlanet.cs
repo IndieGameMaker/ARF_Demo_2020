97be069ca327f831875595874a63548c80a8574d
Lee JaeHyun <myleje@gmail.com>
2020-12-06 11:43:42 +0900

    first commit

---
+++ Assets/02.Scripts/RotatePlanet.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatePlanet : MonoBehaviour
+{
+    public Transform targetTr;
+    public float rotateSpeed = 10.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
